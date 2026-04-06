aed6581a0a2f57828874db845edfabde678c29be
Siddhant Desai <102455826+Skyrim69@users.noreply.github.com>
2026-04-06 23:15:16 +0530

    Delete ReportMerger_py/html_script directory

--- ReportMerger_py/html_script/ReportMerger_py/html_script/core/file_parser.py
+++
@@ -1,8 +0,0 @@
-import copy
-
-class Fileparser:
-
-    def __init__(self, file_soups):
-        self.file_soups = file_soups
-        self.base_file = file_soups[0][1]
-        self.Duplicate_file = copy.deepcopy(self.base_file)
